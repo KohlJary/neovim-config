31aa952994bd6cf5b9c181c278cff52f284e4fc5
Kohlbern Jary <kohlbern.jary@UBC.com>
2023-12-15 13:11:48 -0500

    Tweak treesitter ensure_installed

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter').setup{}
 require'nvim-treesitter.configs'.setup{
-  ensure_installed = { "c_sharp", "angular", "css", "html", "json", "javascript", "lua", "sql", "typescript", "vim", "vimdoc" },
+  ensure_installed = { "c_sharp", "css", "html", "json", "javascript", "lua", "sql", "typescript", "vim" },
   ignore_install = { "smali", "tlaplus" },
   sync_install = false,
   auto_install = true,
